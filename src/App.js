e75088426eb88f2b2245632cf146619e9f9b33f2
Daisugi <volble124@gmail.com>
2020-11-16 01:11:25 +0900

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import Jumbotron from'./components/jumbotron/index';
+import Jumbotron from './components/jumbotron/index';
 import juboData from './fixtures/jumbo.json';
 
 function App() {
   return (
     <Jumbotron.Container>
-      {juboData.map(item => (
-        <Jumbotron key={item.id}direction={item.direction}>
+      {juboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
           <Jumbotron.Pane>
             <Jumbotron.Title>{item.title}</Jumbotron.Title>
             <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
           </Jumbotron.Pane>
           <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt}/>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
           </Jumbotron.Pane>
         </Jumbotron>
       ))}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'normalize.css';
 import App from './App';
 import { GlobalStyles } from './global-styles';
 import reportWebVitals from './reportWebVitals';
 
-render(<>
-  <GlobalStyles />
-  <App />
+render(
+  <>
+    <GlobalStyles />
+    <App />
   </>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
 
 reportWebVitals();
