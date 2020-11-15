e7ae986a43b28a0bfb99460645b522275e559eea
Daisugi <volble124@gmail.com>
2020-11-15 23:14:38 +0900

    add global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { GlobalStyles } from './global-styles';
 import reportWebVitals from './reportWebVitals';
 
-render(<App />, document.getElementById('root'));
+render(<>
+  <GlobalStyles />
+  <App />
+  </>,
+  document.getElementById('root'));
 
 reportWebVitals();
